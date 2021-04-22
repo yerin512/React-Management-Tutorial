a8f05e3802a875a7a23e4f0a8d270fad21add308
yerin512 <sosu0512@naver.com>
2021-04-22 10:23:19 +0900

    이벤트 핸들링 (onClick & onKeyUp)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const onSubmit = ()=> alert('hello');
+  const onKeyUp = (event) => {
+    if(event.keyCode === 13){
+      onSubmit();
+    }
+  };
+
   return (
     <div className="App">
+      <input onKeyUp={onKeyUp} />
+      <button onClick={onSubmit}>
+        Submit
+      </button>
     </div>
   );
 }
