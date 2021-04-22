885133719108125ca4c6b6e2d5aec0b327f6c691
yerin512 <sosu0512@naver.com>
2021-04-22 14:22:01 +0900

    리액트 반복문 .map()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [condition, setCondition] = useState(false);
-  const toggle = ()=> {
-    setCondition(!condition);
-  }
-  useEffect(()=>{
-    console.log(condition);
-
-  },[condition])
-
-  const renderCondition = condition ? 'True' : 'false'
-
+  const Movies = [
+    { title : '괴물1', year: 2002 },
+    { title : '괴물2', year: 2003 },
+    { title : '괴물3', year: 2004 },
+    { title : '괴물4', year: 2005 },
+  ]
+  const renderMovies = Movies.map(movie => {
+    return (
+      <div className="movie" key={movie.title}>
+      <div className="movie-title">{movie.title}</div>
+      <div className="movie-year">{movie.year}</div>
+    </div>
+    );
+  })
   return(
     <div className="App">
-      <h1>Kossie Coder</h1>
-      <div>{renderCondition}</div>
-      <button onClick={toggle}>Toggle</button>
+      <h1>Movie List</h1>
+      {renderMovies}
     </div>
   )
   
