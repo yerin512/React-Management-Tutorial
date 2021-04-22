9fa862d316d01886b47d1a0e55489d331d489de9
yerin512 <sosu0512@naver.com>
2021-04-22 15:39:12 +0900

    영화리스트 추가 폼 만들기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import './App.css';
-import React from 'react';
 import Movie from './components/Movie'
+import React, { useState } from 'react';
 
 function App() {
- 
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieYear, setMovieYear] = useState('');
+  const [movies, setMovies] = useState([
+    { title : '괴물1', year: 2002 },
+    { title : '괴물2', year: 2003 },
+    { title : '괴물3', year: 2004 },
+    { title : '괴물4', year: 2005 },
+  ]);
+
+  const renderMovies = movies.map(movie => {
+    return(
+      <Movie movie={movie} key={movie.title}/>
+  );
+});
+
+  const addMovie = (event) => {
+    event.preventDefault();
+    setMovies([
+      ...movies,
+      {
+        title : movieTitle,
+        year : movieYear,
+    }]);
+
+    if (setMovieYear && setMovieTitle === ''){
+      alert('영화제목과 년도를 입력해주세요')
+    }
+
+    setMovieTitle('');
+    setMovieYear('');
+    
+  };
   return(
-    <div className="App">
-      <h1>Movie List</h1>
-      <Movie/>
-    </div>
+      <div className="App">
+        <h1>Movie List</h1>
+        <form onSubmit={addMovie}>
+        <input
+          type="text"
+          placeholder="영화제목"
+          value={movieTitle}
+          onChange={e => setMovieTitle(e.target.value)}/><br />
+        <input
+          type="text"
+          placeholder="영화년도"
+          value={movieYear}
+          onChange={e => setMovieYear(e.target.value)}/><br />
+          <button type="submit">영화추가</button>
+      </form>
+         {renderMovies}
+          
+      </div>
   )
-  
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,9 @@
-export default function Movie(){
+export default function Movie({movie}){
 
-     const Movies = [
-    { title : '괴물1', year: 2002 },
-    { title : '괴물2', year: 2003 },
-    { title : '괴물3', year: 2004 },
-    { title : '괴물4', year: 2005 },
-  ]
-  const renderMovies = Movies.map(movie => {
-      return(
+  return(
     <div className="movie" key={movie.title}>
         <div className="movie-title">{movie.title}</div>
         <div className="movie-year">{movie.year}</div>
     </div>
-    );
-  });
-   
-  return(
-        <>
-         {renderMovies}
-        </>
     )
 }
