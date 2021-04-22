67d829c7df5f90a4eced6c717164967b0da82f11
yerin512 <sosu0512@naver.com>
2021-04-22 11:39:06 +0900

    폼에서useState사용하기 event.preventDefault();

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import React, { useState } from 'react';
 
 
 function App() {
-  const [name, setName] = useState('yerin');
-
+  const onSubmit = (event)=> {
+    event.preventDefault();
+    console.log(userName, password);
+  };
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
   return (
+   <form onSubmit={onSubmit}>
     <div className="App">
-
-      <button onClick={() => {
-        setName(name === "yerin" ? "Ahn" : "yerin")}}>Chane Name</button>
-      <span>{name}</span>
-    
+      <input 
+      placeholder="Username"
+      value={userName}
+      onChange={(e)=>setUserName(e.target.value)}
+      /><br />
+      <input 
+      placeholder ="Password"
+      value={password}
+      onChange={(e)=>setPassword(e.target.value)}
+      /><br />
+      <button type="submit">Login</button>
     </div>
+    </form>  
   );
 }
 
