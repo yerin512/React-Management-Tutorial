59a514f3de25cf65cd60fa2549296d71b00a94ec
yerin512 <sosu0512@naver.com>
2021-04-28 16:20:51 +0900

    유저데이터화면에출력하기

---
+++ src/components/UserList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const UserList = ({ users }) => {
+    console.log(users)
+    return(
+        <div>
+            {users.map(user => {
+                return (<div key={user.id}>
+                    {user.name}
+                </div>);
+            })}
+        </div>
+    );
+};
+
+export default UserList;
--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import UserList from '../UserList';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then(response => {
-                console.log(response)
+                setUsers(response.data);
             });
     }, []);
 
     return(
-        <h1>Users</h1>
+        <>
+            <h1>Users</h1>
+            <UserList users= {users}/>
+        </>
     );
 };
 
