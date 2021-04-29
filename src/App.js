a9725820c282861270221cf4611aadf2bd5e441a
yerin512 <sosu0512@naver.com>
2021-04-29 10:52:52 +0900

    movies, home 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Movie from './components/Movie';
-import Users from './components/pages/Users'
-import React, { useState } from 'react';
-import MovieForm from './components/MovieForm';
+
 import Navbar from './components/Navbar'
+import Users from './components/pages/Users'
+import Home from './components/pages/Home';
+import Movies from './components/pages/Movies';
 
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const removeMovie = (id) => {
-    setMovies(movies.filter(movie => {
-      return movie.id !== id;
-    }));
-  };
-
-  const renderMovies = movies.length ? movies.map(movie => {
-    return (
-      <Movie
-        movie={movie}
-        key={movie.id}
-        removeMovie={removeMovie}
-      />
-    );
-  })
-    : '추가된 영화가 없습니다.';
-
-  const addMovie = (movie) => {
-    setMovies([
-      ...movies,
-      movie
-    ]);
-
-
-  };
+ 
   return (
     <Router>
       <div className="App">
@@ -42,15 +17,14 @@ function App() {
         <div className="container">
         <Switch>
           <Route path="/movies">
-            <h1>Movie List</h1>
-            <MovieForm addMovie={addMovie} />
-            {renderMovies}
+            <Movies />
+           
           </Route>
           <Route path="/users">
             <Users />
           </Route>
           <Route path="/" exact>
-            <h1>Home</h1>
+            <Home />
           </Route>
         </Switch>
         </div>
